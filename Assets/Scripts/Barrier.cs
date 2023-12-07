201ac624b508d07036735a07089d785bdaf0efdf
AndXrey <and.grigoryev2017@gmail.com>
2023-12-08 01:12:22 +0300

    newUpdate08.12

--- Assets/Scripts/Barrier.cs
+++ Assets/Scripts/Barrier.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Barrier : MonoBehaviour
 {
@@ -13,19 +14,37 @@ public class Barrier : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < 150)
+        if (SceneManager.GetActiveScene().name == "SampleScene")
         {
-            if (transform.position.z > -0.5f)
+            if (transform.position.x < 150)
             {
+                if (transform.position.z > -0.5f)
                 {
                     transform.position = new Vector3(transform.position.x, transform.position.y, -0.5f);
                 }
             }
-        }
-        if (transform.position.z < -10)
-        {
+            if (transform.position.z < -10)
             {
                 transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Demo 1")
+        {
+            if (transform.position.z > 30)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, 30);
+            }
+            if (transform.position.z < 12)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, 12);
+            }
+            if (transform.position.x < -30)
+            {
+                transform.position = new Vector3(-30, transform.position.y, transform.position.z);
+            }
+            if (transform.position.x > 25)
+            {
+                transform.position = new Vector3(25, transform.position.y, transform.position.z);
             }
         }
 
