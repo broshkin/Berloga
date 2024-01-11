361bb60a9ebebe734d47806a2ac71095c8fefc90
AndXrey <and.grigoryev2017@gmail.com>
2024-01-11 17:09:07 +0300

    11.01

--- Assets/Scripts/EnterInFortress.cs
+++ Assets/Scripts/EnterInFortress.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class EnterInFortress : MonoBehaviour
 {
@@ -13,6 +14,22 @@ public class EnterInFortress : MonoBehaviour
         if (other.tag == "Player")
         {
             canvas.SetActive(true);
+            if (gameObject.tag == "FortressEnter")
+            {
+                canvas.GetComponentInChildren<TextMeshProUGUI>().text = "Войти в подземелье\nНажмите клавишу 'E'";
+            }
+            if (gameObject.tag == "FortressExit")
+            {
+                canvas.GetComponentInChildren<TextMeshProUGUI>().text = "Выйти из подземелья\nНажмите клавишу 'E'";
+            }
+            if (gameObject.tag == "AltarEnter")
+            {
+                canvas.GetComponentInChildren<TextMeshProUGUI>().text = "Войти в алтарь\nНажмите клавишу 'E'";
+            }
+            if (gameObject.tag == "AltarExit")
+            {
+                canvas.GetComponentInChildren<TextMeshProUGUI>().text = "Выйти из алтаря\nНажмите клавишу 'E'";
+            }
         }
     }
     private void OnTriggerExit(Collider other)
@@ -25,10 +42,25 @@ public class EnterInFortress : MonoBehaviour
     private void Update()
     {
         if (canvas.activeSelf)
-        {
+        {    
             if (Input.GetKeyDown(KeyCode.E))
             {
-                SceneManager.LoadScene("Demo 3");
+                if (gameObject.tag == "FortressEnter")
+                {
+                    SceneManager.LoadScene(4);
+                }
+                if (gameObject.tag == "FortressExit")
+                {
+                    SceneManager.LoadScene(3);
+                }
+                if (gameObject.tag == "AltarEnter")
+                {
+                    SceneManager.LoadScene(5);
+                }
+                if (gameObject.tag == "AltarExit")
+                {
+                    SceneManager.LoadScene(4);
+                }
             }
         }
     }
