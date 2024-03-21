b80e4c07139dd0cd08ae77c7fa713aef3aff470a
mrrent <and.grigoryev2017@gmail.com>
2024-03-21 19:00:44 +0300

    123

--- Assets/Scripts/GetArtefactCanvas.cs
+++ Assets/Scripts/GetArtefactCanvas.cs
@@ -8,7 +8,7 @@ public class GetArtefactCanvas : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(false);
+ 
     }
 
     // Update is called once per frame
